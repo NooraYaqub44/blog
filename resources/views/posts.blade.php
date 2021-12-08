0741d0a85cbfe025d26733146e865c91e51fa9f1
noora <noorayaqub44@gmail.com>
2021-12-08 13:55:47 -0800

    first v- of the blog'

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <title>Blog Post</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post"> My first Blog</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Non temporibus molestiae reprehenderit maxime perspiciatis
+            commodi inventore cupiditate ad quisquam esse, est facilis,
+            maiores nemo voluptates, dolores corporis repellendus nihil
+            officia!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post"> My Second Blog</a></h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Non temporibus molestiae reprehenderit maxime perspiciatis
+            commodi inventore cupiditate ad quisquam esse, est facilis,
+            maiores nemo voluptates, dolores corporis repellendus nihil
+            officia!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post"> My third Blog</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Non temporibus molestiae reprehenderit maxime perspiciatis
+            commodi inventore cupiditate ad quisquam esse, est facilis,
+            maiores nemo voluptates, dolores corporis repellendus nihil
+            officia!
+        </p>
+    </article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get ('/', function () {
+    return view('posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+
+
+    if (! file_exists($path = __DIR__. "/../resources/posts/{$slug}.html")) {
+        return redirect('/');
+    }
+
+   $post= Cache() -> remember("posts.{$slug}", 1200, function () use ($path) {
+        return file_get_contents($path);
+    });
+
+
+
+    return view('post', [
+        'post' =>$post
+    ]);
+})-> where('post', '[A-z_\-]+');
